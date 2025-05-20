e56e0585d319d4fa65c90e736028334df4442cfd
MDBBee <bobbyugbebor@gmail.com>
2025-05-20 12:02:21 +0300

    Profile page after login/signup

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,21 @@
 import { LuUser2 } from 'react-icons/lu';
+import { fetchProfileImage } from '@/utils/actions';
+import Image from 'next/image';
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage)
+    return (
+      <Image
+        src={profileImage}
+        alt="Profile Image"
+        className="w-7 h-7 rounded-full object-cover"
+        width={28}
+        height={28}
+        priority
+      />
+    );
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
--- utils/actions.ts
+++ utils/actions.ts
@@ -2,23 +2,88 @@
 
 import { profileSchema } from './schemas';
 
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// Utils funcs
+// a) get auth user
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+  return user;
+};
+// b) fetch profile
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) return redirect('/profile/create');
+  return profile;
+};
+// c) Fetch Image
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
+};
+
+// Create Profile
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
 ) => {
   try {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
+    const user = await currentUser();
+    // console.log('USER✅✅', user);
+    if (!user) throw new Error('Please login to create a profile');
 
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: 'Profile Created' };
-  } catch (error: any) {
-    console.log(JSON.stringify(error, null, 2));
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
     return {
-      message: `Field: ${error.issues[0].path[0]}\n \tError: ${error.issues[0].message}`,
+      message: error instanceof Error ? error.message : 'An error occurred',
     };
   }
+  redirect('/');
+};
+
+// Fetch Profile
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: 'update profile action' };
 };
